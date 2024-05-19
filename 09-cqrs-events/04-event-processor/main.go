10764bbe551125a022ab871e49b92b1e315e2368
EJ Agas <agas.ej09@gmail.com>
2024-05-19 17:35:26 +0800

    configure and implement event processor
    Signed-off-by: EJ Agas <agas.ej09@gmail.com>

---
+++ 09-cqrs-events/04-event-processor/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill/components/cqrs"
+	"github.com/ThreeDotsLabs/watermill/message"
+)
+
+func RegisterEventHandlers(
+	sub message.Subscriber,
+	router *message.Router,
+	handlers []cqrs.EventHandler,
+	logger watermill.LoggerAdapter,
+) error {
+	eventProcessor, err := cqrs.NewEventProcessorWithConfig(
+		router,
+		cqrs.EventProcessorConfig{
+			GenerateSubscribeTopic: func(params cqrs.EventProcessorGenerateSubscribeTopicParams) (string, error) {
+				return params.EventName, nil
+			},
+			SubscriberConstructor: func(params cqrs.EventProcessorSubscriberConstructorParams) (message.Subscriber, error) {
+				return sub, nil
+			},
+			Marshaler: cqrs.JSONMarshaler{
+				GenerateName: cqrs.StructName,
+			},
+			Logger: logger,
+		},
+	)
+
+	if err != nil {
+		return fmt.Errorf("could not create event subscriber: %w", err)
+	}
+
+	if err := eventProcessor.AddHandlers(handlers...); err != nil {
+		return fmt.Errorf("could not add event handlers: %w", err)
+	}
+
+	return nil
+}
